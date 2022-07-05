cd2f0ebd2a43e5e8c6311bc717f84fb6dcf5b880
L-Karimi <luciahkei101@gmail.com>
2022-07-05 08:31:40 +0300

    added js modal function

--- scripts.js
+++ scripts.js
@@ -6,7 +6,6 @@ const modalCloseBtn = document.querySelector('[data-modal-close]');
 const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
 
 // modal function
-const modalCloseFunc = function () { modal.classList.add('closed') }
 
 // modal eventListener
 modalCloseOverlay.addEventListener('click', modalCloseFunc);
@@ -36,7 +35,23 @@ const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-bt
 const overlay = document.querySelector('[data-overlay]');
 
 
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
 
+  // mobile menu function
+  const mobileMenuCloseFunc = function () {
+    mobileMenu[i].classList.remove('active');
+    overlay.classList.remove('active');
+  }
+
+  mobileMenuOpenBtn[i].addEventListener('click', function () {
+    mobileMenu[i].classList.add('active');
+    overlay.classList.add('active');
+  });
+
+  mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+  overlay.addEventListener('click', mobileMenuCloseFunc);
+
+}
 
 
 
