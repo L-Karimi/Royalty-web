2849bfe060670e77d7d45173ddffc33aaae4861d
L-Karimi <luciahkei101@gmail.com>
2022-07-05 08:34:20 +0300

    modified colors

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const modalCloseBtn = document.querySelector('[data-modal-close]');
 const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
 
 // modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
 
 // modal eventListener
 modalCloseOverlay.addEventListener('click', modalCloseFunc);
